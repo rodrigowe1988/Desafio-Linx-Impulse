755bc7cd57df2b69bb853e9032fad917e0ea6006
Rodrigo Weber <60714212+rodrigowe1988@users.noreply.github.com>
2020-11-16 13:01:28 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ fetch("https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1"
     resposta.json().then((respostaJson) => {
         const listagemProdutos = document.querySelector(".listaDeProdutos")
         const dados = respostaJson.products;
-        bringProductsFromAPI(dados, listagemProdutos)
+        montarProdutos(dados, listagemProdutos)
     })
 })
 
-function bringProductsFromAPI(products, elemento) {
-    for (const product of products) {
+function montarProdutos(produtos, elemento) {
+    for (const produto of produtos) {
         const li = document.createElement("li");
-        li.innerHTML = `<div class="products1">
-        <img class="productImage" src="http:${product.image}">
-            <h3 class="productNamae">${product.name}</h3>
-            <p class="productDescription">${product.description}</p>
-            <p class="firstPrice">R$ ${product.oldPrice}</p>
-            <p class="finalPrice">R$ ${product.price}</p>
-            <p class="partPrice">ou ${product.installments.count}x de R$
-            ${product.installments.value}</p>
+        li.innerHTML = `<div class="produto-1">
+        <img class="imagemProduto" src="http:${produto.image}">
+            <h3 class="nomeDoProduto">${produto.name}</h3>
+            <p class="descricaoDoProduto">${produto.description}</p>
+            <p class="precoInicial">R$ ${produto.oldPrice}</p>
+            <p class="precoFinal">R$ ${produto.price}</p>
+            <p class="precoParcelado">ou ${produto.installments.count}x de R$
+            ${produto.installments.value}</p>
             <input type="button" value="Comprar" class="botaoComprar">`;
 
         elemento.append(li)
@@ -24,3 +24,16 @@ function bringProductsFromAPI(products, elemento) {
     }
 
 }
+
+const refreshButton = document.querySelector('.botao')
+
+refreshButton.addEventListener("click", () =>{
+    fetch("https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1").then((resposta) => {
+    resposta.json().then((respostaJson) => {
+        const listagemProdutos = document.querySelector(".listaDeProdutos")
+        const dados = respostaJson.products;
+        montarProdutos(dados, listagemProdutos)
+    })
+})
+  
+})
